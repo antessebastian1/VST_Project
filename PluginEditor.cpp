07c518bf240d87e6e5a383c368999793abe8a64f
antesnicolas <nicolasalexanderantes@gmail.com>
2023-04-03 18:42:13 +0200

    changed gainboxtextcolour to red

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -22,7 +22,7 @@ WavetableSynthAntesAudioProcessorEditor::WavetableSynthAntesAudioProcessorEditor
     //boundary border for Gain
     gainBox.setText("Gain");
     gainBox.setColour(gainBox.outlineColourId, juce::Colours::red.withAlpha(0.2f));
-    gainBox.setColour(gainBox.textColourId, juce::Colours::aliceblue);
+    gainBox.setColour(gainBox.textColourId, juce::Colours::red);
     addAndMakeVisible(gainBox);
 
     //gain slider
